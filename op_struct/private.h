34747ff7b234218cf8c07ca65847a718d7e8fe33
Enzo Cornaggia <enzo.cornaggia@etu.u-bordeaux.fr>
2022-07-01 15:39:03 +0200

    organize with folder

---
+++ op_struct/private.h
@@ -0,0 +1,16 @@
+#ifndef __PRIVATE_H__
+#define __PRIVATE_H__
+
+#include <string.h>
+
+struct op_s {
+    char* num1;
+    char* num2;
+    char* result;
+};
+
+typedef struct op_s op_s;
+
+#endif
+
+
